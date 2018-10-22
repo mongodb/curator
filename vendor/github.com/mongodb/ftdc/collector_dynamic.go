5bb0590a696cdf8a4b078900ebe802b2bdc659ea
julianedwards <julian.edwards@mongodb.com>
2018-10-22 16:31:58 -0400

    vendoring

---
+++ vendor/github.com/mongodb/ftdc/collector_dynamic.go
@@ -0,0 +1,96 @@
+package ftdc
+
+import (
+	"bytes"
+
+	"github.com/mongodb/mongo-go-driver/bson"
+	"github.com/pkg/errors"
+)
+
+type dynamicCollector struct {
+	maxChunkSize int
+	chunks       []*batchCollector
+	hahes        []string
+	currentNum   int
+}
+
+// NewDynamicCollector constructs a Collector that records metrics
+// from documents, creating new chunks when either the size of the
+// metrics payload exceeds the specified max chunk size OR the schema
+// changes.
+//
+// There is some overhead associated with detecting schema changes,
+// particularly for documents with more complex schemas, so you may
+// wish to opt for a simpler collector in some cases.
+func NewDynamicCollector(maxChunkSize int) Collector {
+	return &dynamicCollector{
+		maxChunkSize: maxChunkSize,
+		chunks: []*batchCollector{
+			newBatchCollector(maxChunkSize),
+		},
+	}
+}
+
+func (c *dynamicCollector) Info() CollectorInfo {
+	out := CollectorInfo{}
+	for _, c := range c.chunks {
+		info := c.Info()
+		out.MetricsCount += info.MetricsCount
+		out.PayloadSize += info.PayloadSize
+		out.SampleCount += info.SampleCount
+	}
+	return out
+}
+
+func (c *dynamicCollector) Reset() {
+	c.chunks = []*batchCollector{newBatchCollector(c.maxChunkSize)}
+	c.hahes = []string{}
+}
+
+func (c *dynamicCollector) SetMetadata(d *bson.Document) {
+	c.chunks[0].SetMetadata(d)
+}
+
+func (c *dynamicCollector) Add(doc *bson.Document) error {
+	if len(c.hahes) == 0 {
+		docHash, num := metricsHash(doc)
+		c.hahes = append(c.hahes, docHash)
+		c.currentNum = num
+		return errors.WithStack(c.chunks[0].Add(doc))
+	}
+
+	if len(c.hahes) != len(c.chunks) {
+		// this is (maybe) panic worthy
+		return errors.New("collector is corrupt")
+	}
+
+	lastIdx := len(c.hahes) - 1
+
+	lastChunk := c.chunks[lastIdx]
+	lastHash := c.hahes[lastIdx]
+
+	docHash, num := metricsHash(doc)
+	if lastHash == docHash && c.currentNum == num {
+		return errors.WithStack(lastChunk.Add(doc))
+	}
+
+	chunk := newBatchCollector(c.maxChunkSize)
+	c.chunks = append(c.chunks, chunk)
+	c.hahes = append(c.hahes, docHash)
+
+	return errors.WithStack(chunk.Add(doc))
+}
+
+func (c *dynamicCollector) Resolve() ([]byte, error) {
+	buf := bytes.NewBuffer([]byte{})
+	for _, chunk := range c.chunks {
+		out, err := chunk.Resolve()
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+
+		_, _ = buf.Write(out)
+	}
+
+	return buf.Bytes(), nil
+}
---
+++ vendor/github.com/mongodb/ftdc/iterator_sample.go
@@ -0,0 +1,90 @@
+package ftdc
+
+import (
+	"context"
+
+	"github.com/mongodb/mongo-go-driver/bson"
+)
+
+// sampleIterator provides an iterator for iterating through the
+// results of a FTDC data chunk as BSON documents.
+type sampleIterator struct {
+	closer   context.CancelFunc
+	stream   <-chan *bson.Document
+	sample   *bson.Document
+	metadata *bson.Document
+}
+
+func (c *Chunk) streamFlattenedDocuments(ctx context.Context) <-chan *bson.Document {
+	out := make(chan *bson.Document)
+
+	go func() {
+		defer close(out)
+		for i := 0; i < c.nPoints; i++ {
+			doc := bson.NewDocument()
+
+			for _, m := range c.metrics {
+				doc.Append(bson.EC.Int64(m.Key(), m.Values[i]))
+			}
+
+			select {
+			case out <- doc:
+				continue
+			case <-ctx.Done():
+				return
+			}
+		}
+
+	}()
+
+	return out
+}
+
+func (c *Chunk) streamDocuments(ctx context.Context) <-chan *bson.Document {
+	out := make(chan *bson.Document)
+
+	go func() {
+		defer close(out)
+
+		for i := 0; i < c.nPoints; i++ {
+			doc, _ := rehydrateDocument(c.reference, i, c.metrics, 0)
+			select {
+			case <-ctx.Done():
+				return
+			case out <- doc:
+				continue
+			}
+		}
+	}()
+
+	return out
+}
+
+// Close releases all resources associated with the iterator.
+func (iter *sampleIterator) Close()     { iter.closer() }
+func (iter *sampleIterator) Err() error { return nil }
+
+func (iter *sampleIterator) Metadata() *bson.Document { return iter.metadata }
+
+// Document returns the current document in the iterator. It is safe
+// to call this method more than once, and the result will only be nil
+// before the iterator is advanced.
+func (iter *sampleIterator) Document() *bson.Document { return iter.sample }
+
+// Next advances the iterator one document. Returns true when there is
+// a document, and false otherwise.
+func (iter *sampleIterator) Next(ctx context.Context) bool {
+	for {
+		select {
+		case <-ctx.Done():
+			return false
+		case doc := <-iter.stream:
+			if doc == nil {
+				return false
+			}
+
+			iter.sample = doc
+			return true
+		}
+	}
+}
