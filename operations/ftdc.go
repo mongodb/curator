50ad8dca6c52aa7f215b101dd7c2b72440cffd06
julianedwards <julian.edwards@mongodb.com>
2018-10-22 15:38:54 -0400

    add ftdc operations file

---
+++ operations/ftdc.go
@@ -0,0 +1,58 @@
+package operations
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/mongodb/ftdc"
+	"github.com/pkg/errors"
+	"github.com/urfave/cli"
+)
+
+func FTDC() cli.Command {
+	return cli.Command{
+		Name:  "ftdc",
+		Usage: "tools for running FTDC parsing and generating tools",
+		Flags: []cli.Flag{},
+		Subcommands: []cli.Command{
+			ftdcDump(),
+		},
+	}
+}
+
+func ftdcDump() cli.Command {
+	return cli.Command{
+		Name:  "dump",
+		Usage: "dump ftdc data from a given BSON file",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "path",
+				Usage: "dump FTDC data from this file",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			path := c.String("path")
+			if path == "" {
+				return errors.New("path is not specified")
+			}
+
+			f, err := os.Open(path)
+			if err != nil {
+				return errors.Wrapf(err, "problem opening file '%s'", path)
+			}
+			defer f.Close()
+
+			iter := ftdc.ReadMetrics(ctx, f)
+
+			for iter.Next(ctx) {
+				fmt.Println(iter.Document().ToExtJSON(false))
+			}
+
+			return iter.Err()
+		},
+	}
+}
