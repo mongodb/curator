f26da9963976f6e4809a3fbc33a53b198e5d741a
julianedwards <julian.edwards@mongodb.com>
2018-11-01 17:28:41 -0400

    revendor ftdc

---
+++ vendor/github.com/mongodb/ftdc/collector.go
@@ -0,0 +1,43 @@
+package ftdc
+
+import (
+	"github.com/mongodb/mongo-go-driver/bson"
+)
+
+// Collector describes the interface for collecting and constructing
+// FTDC data series. Implementations may have different efficiencies
+// and handling of schema changes.
+type Collector interface {
+	// SetMetadata sets the metadata document for the collector or
+	// chunk. This document is optional. Pass a nil to unset it,
+	// or a different document to override a previous operation.
+	SetMetadata(*bson.Document)
+
+	// Add extracts metrics from a document and appends it to the
+	// current collector. These documents MUST all be
+	// identical. Returns an error if there is a problem parsing
+	// the document or if the number of statistics collected changes.
+	Add(*bson.Document) error
+
+	// Resolve renders the existing documents and outputs the full
+	// FTDC chunk as a byte slice to be written out to storage.
+	Resolve() ([]byte, error)
+
+	// Reset clears the collector for future use.
+	Reset()
+
+	// Info reports on the current state of the collector for
+	// introspection and to support schema change and payload
+	// size.
+	Info() CollectorInfo
+}
+
+// CollectorInfo reports on the current state of the collector and
+// provides introspection into the current state of the collector for
+// testing, transparency, and to support more complex collector
+// features, including payload size controls and schema change
+type CollectorInfo struct {
+	MetricsCount int
+	SampleCount  int
+	PayloadSize  int
+}
---
+++ vendor/github.com/mongodb/ftdc/util.go
@@ -0,0 +1,51 @@
+package ftdc
+
+import (
+	"bytes"
+	"compress/zlib"
+	"encoding/binary"
+)
+
+func undelta(value int64, deltas []int64) []int64 {
+	out := make([]int64, len(deltas))
+	for idx, delta := range deltas {
+		out[idx] = value + delta
+		value = out[idx]
+	}
+	return out
+}
+
+func encodeSizeValue(val uint32) []byte {
+	tmp := make([]byte, 4)
+
+	binary.LittleEndian.PutUint32(tmp, val)
+
+	return tmp
+}
+
+func encodeValue(val int64) []byte {
+	tmp := make([]byte, binary.MaxVarintLen64)
+	num := binary.PutVarint(tmp, val)
+	return tmp[:num]
+}
+
+func compressBuffer(input []byte) ([]byte, error) {
+	buf := bytes.NewBuffer([]byte{})
+	zbuf := zlib.NewWriter(buf)
+
+	var err error
+
+	buf.Write(encodeSizeValue(uint32(len(input))))
+
+	_, err = zbuf.Write(input)
+	if err != nil {
+		return nil, err
+	}
+
+	err = zbuf.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
