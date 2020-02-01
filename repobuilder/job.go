6075f67217d3824f65130460ec336b01fe0b6ed4
tycho garen <garen@tychoish.com>
2020-02-01 16:16:48 -0500

    MAKE-1098: allow options creation for jobs

--- repobuilder/job.go
+++ repobuilder/job.go
@@ -73,18 +73,54 @@ func buildRepoJob() *repoBuilderJob {
 }
 
 // NewBuildRepoJob constructs a repository building job, which
-// implements the amboy.Job interface.
+// implements the amboy.Job interface. Provides a legacy interface for
+// NewRepoBuilderJob.
 func NewBuildRepoJob(conf *RepositoryConfig, distro *RepositoryDefinition, version, arch, profile string, pkgs ...string) (amboy.Job, error) {
-	var err error
+	return NewRepoBuilderJob(RepoBuilderJobOptions{
+		Configuration: conf,
+		Distro:        distro,
+		Version:       version,
+		Arch:          arch,
+		Profile:       profile,
+		Packages:      pkgs,
+		JobID:         fmt.Sprint(job.GetNumber()),
+	})
+}
+
+// RepoBuilderJobOptions describes the options to construct a
+// RepoBuilderJob.
+type RepoBuilderJobOptions struct {
+	Configuration *RepositoryConfig     `bson:"conf" json:"conf" yaml:"conf"`
+	Distro        *RepositoryDefinition `bson:"distro" json:"distro" yaml:"distro"`
+	Version       string                `bson:"version" json:"version" yaml:"version"`
+	Arch          string                `bson:"arch" json:"arch" yaml:"arch"`
+	Profile       string                `bson:"profile" json:"profile" yaml:"profile"`
+	Packages      []string              `bson:"packages" json:"packages" yaml:"packages"`
+	JobID         string                `bson:"job_id" json:"job_id" yaml:"job_id"`
+}
+
+func (opts *RepoBuilderJobOptions) Validate() error {
+	catcher := grip.NewBasicCatcher()
+	catcher.NewWhen(opts.Configuration == nil, "configuration must not be nil")
+	catcher.NewWhen(opts.Distro == nil, "distro specification must not be nil")
+	catcher.NewWhen(len(packages) == 0, "must specify one or more packages")
+
+	return catcher.Resolve()
+}
+
+// NewRepoBuilderJob produces a new repo job.
+func NewRepoBuilderJob(opts RepoBuilderJobOptions) (amboy.Job, error) {
+	err := opts.Validate()
+	err
 
 	j := buildRepoJob()
 
-	j.release, err = bond.NewMongoDBVersion(version)
+	j.release, err = bond.NewMongoDBVersion(opts.Version)
 	if err != nil {
 		return nil, err
 	}
 
-	j.Conf = conf
+	j.Conf = opts.Configuration
 	if j.Conf.WorkSpace == "" {
 		j.Conf.WorkSpace, err = os.Getwd()
 		if err != nil {
@@ -92,13 +128,12 @@ func NewBuildRepoJob(conf *RepositoryConfig, distro *RepositoryDefinition, versi
 		}
 	}
 
-	j.SetID(fmt.Sprintf("build-%s-repo.%d", distro.Type, job.GetNumber()))
-	j.Arch = distro.getArchForDistro(arch)
-	j.Distro = distro
-	j.Conf = conf
-	j.PackagePaths = pkgs
-	j.Version = version
-	j.Profile = profile
+	j.SetID(fmt.Sprintf("%s.distro.%s.repo.%s", jobName, distro.Type, opts.JobID))
+	j.Arch = opts.Distro.getArchForDistro(opts.Arch)
+	j.Distro = opts.Distro
+	j.PackagePaths = opts.Packages
+	j.Version = opts.Version
+	j.Profile = opts.Profile
 
 	return j, nil
 }
