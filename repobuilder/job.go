cc90791bbd430e7011a90e64116adc8fb27a3289
Kim Tao <kim.tao@mongodb.com>
2020-03-20 17:08:46 -0400

    Download packages within /data/tmp (#102)

--- repobuilder/job.go
+++ repobuilder/job.go
@@ -207,7 +207,11 @@ func (j *repoBuilderJob) setup() {
 		}
 	}
 
-	j.tmpdir, err = ioutil.TempDir("", j.ID())
+	if err = os.MkdirAll("/data/tmp"); err != nil {
+		j.AddError(errors.Wrap(err, "could not create temp directory"))
+		return
+	}
+	j.tmpdir, err = ioutil.TempDir("/data/tmp", j.ID())
 	if err != nil {
 		j.AddError(errors.Wrap(err, "problem making tempdir"))
 	}
