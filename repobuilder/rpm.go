b3c7676f1b55fb861e8c9832b44fd8b4a95abe6f
julianedwards <julian.edwards@mongodb.com>
2020-06-03 15:19:06 -0400

    fix build and use correct filepath

--- repobuilder/rpm.go
+++ repobuilder/rpm.go
@@ -51,8 +51,8 @@ func (j *rpmRepoBuilder) rebuildRepo(workingDir string) error {
 	} else {
 		// remove olddata before running createrepo or else the command
 		// will fail if it exists.
-		if err = os.RemoveAll(".olddata"); err != nil {
-			return errors.Wrap("problem removing .olddata dir", err)
+		if err = os.RemoveAll(filepath.Join(workingDir, ".olddata")); err != nil {
+			return errors.Wrap(err, "problem removing .olddata dir")
 		}
 
 		grip.Notice(message.Fields{
